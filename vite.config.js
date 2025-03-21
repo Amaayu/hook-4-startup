424e16c3a05c0dc6231d4bb2d49bbca4ab6572f8
Amaayu <namdevaniket446@gmail.com>
2025-03-21 20:58:21 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-//https://hook4startup-bakend-java-dev.onrender.com
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://hook4startup-bakend-java-dev.onrender.com", // ⚡️ Backend ka URL
-        changeOrigin: true,
-        secure: true, // Agar https pe issue ho to
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
