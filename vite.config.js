b58479a61b0d197b4f6aa7c95200173a8b3a9d12
Amaayu <namdevaniket446@gmail.com>
2025-03-21 15:02:28 +0530

    final done

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
   preview: {
     port: 4173, // or any port you prefer
     host: "0.0.0.0", // Important for Docker
+    allowedHosts: ["hook-4-startup.onrender.com"], // ✅ Add Render domain here
+  },
+  server: {
+    host: "0.0.0.0", // Docker ke liye zaroori
+    port: 3000, // Ya koi bhi port jo tum use kar rahe ho
   },
 });
