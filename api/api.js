d479db25853615ec7d8100053dc3737f06897517
Aayush tonk <118004037+Amaayu@users.noreply.github.com>
2025-03-22 22:22:59 +0530

    Update api.js

--- api/api.js
+++ api/api.js
@@ -1,5 +1,5 @@
 //const api = "https://hook4startup-bakend-java-dev.onrender.com"; // ✅ API URL
 //const api = "http://localhost:8081";
 //http://host.docker.internal:8081
-const api = "http://localhost:8081";
+const api = "https://hook4startup-bakend.onrender.com";
 export default api;
